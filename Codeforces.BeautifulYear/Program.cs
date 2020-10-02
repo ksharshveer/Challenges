81cdb00249773d52940a6bb208c09e213a2322d9
Harshveer Singh <ksharshveer@gmail.com>
2020-10-02 04:40:14 -0700

    Solved Beautiful Year

---
+++ Codeforces.BeautifulYear/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Codeforces.BeautifulYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var year = Console.ReadLine();
+            Console.WriteLine(BeautifulYear(year));
+        }
+
+        static string BeautifulYear(string currentYear)
+        {
+            var y = Convert.ToInt32(currentYear);
+            short[] nums = new short[currentYear.Length];
+            while (++y < 10000)
+            {
+                var yStr = y.ToString();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    nums[i] = Convert.ToInt16(yStr.Substring(i, 1));
+                }
+                if (nums.ToImmutableHashSet().Count < currentYear.Length) continue;
+                return yStr;
+            }
+            return currentYear;
+        }
+
+    }
+}
