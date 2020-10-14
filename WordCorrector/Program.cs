57ca2821a1dd400e4770b52be4e16674a4156f1e
Harshveer Singh <ksharshveer@gmail.com>
2020-10-14 13:55:42 -0700

    Solved WordCorrector.
    Corrects the asthetics of a word to its likely proper lowercase or uppercase variant.

---
+++ WordCorrector/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WordCorrector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Solve(input);
+        }
+
+        private static void Solve(string input)
+        {
+            int lowerCaseCount = 0;
+            int half = (int) Math.Ceiling(input.Length / (2.0));
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLower(input[i])) lowerCaseCount += 1;
+            }
+
+            if (lowerCaseCount >= half) Console.WriteLine(input.ToLower());
+            else Console.WriteLine(input.ToUpper());
+        }
+
+    }
+}
