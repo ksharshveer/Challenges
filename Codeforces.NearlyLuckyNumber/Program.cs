cbf31f3ccc9a8536b588a94ba1e95e48ffcf7369
Harshveer Singh <ksharshveer@gmail.com>
2020-10-20 04:19:47 -0700

    Solved NearlyLuckyNumber
    Notes! Be careful about char <-> int conversions

---
+++ Codeforces.NearlyLuckyNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codeforces.NearlyLuckyNumber
+{
+    class Program
+    {
+        static HashSet<char> luckyNumbers = new HashSet<char> { '4', '7' };
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(Solve(ref input));
+        }
+
+        private static string Solve(ref string input)
+        {
+
+            var luckyNumsCount = 0;
+            foreach (var c in input)
+            {
+                if (luckyNumbers.Contains(c)) luckyNumsCount++;
+            }
+
+            if (luckyNumbers.Contains(luckyNumsCount.ToString()[0])) return "YES";
+            return "NO";
+        }
+    }
+}
