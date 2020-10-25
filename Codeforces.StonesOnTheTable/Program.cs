67a3a3d4728ee3d859e7bbaa915ed3ca50e4296f
Harshveer Singh <ksharshveer@gmail.com>
2020-10-25 04:28:32 -0700

    Solved StonesOnTheTable ...
    Used discard operator '_'
    Note! Don't specify a type for '_' or it becomes a normal variable.
    
    Untested Commented Code - Multiple assignment/initialization in the for loop for scoped variable usage.

---
+++ Codeforces.StonesOnTheTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Codeforces.StonesOnTheTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = Console.ReadLine();
+            var input = Console.ReadLine();
+            Console.WriteLine(Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            if (input.Length < 2) return 0;
+
+            var minMoves = 0;
+            //for (var (current, i) = (input[0], 1); i < input.Length; i++)
+            char current = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == current) minMoves += 1;
+                else current = input[i];
+            }
+
+            return minMoves;
+        }
+    }
+}
