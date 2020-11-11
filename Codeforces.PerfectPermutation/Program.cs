0fd94943df42c05e2e47f68f3fd41f6a6ba49ce2
Harshveer Singh <ksharshveer@gmail.com>
2020-11-11 14:09:01 -0800

    Solved PerfectPermutation.

---
+++ Codeforces.PerfectPermutation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Codeforces.PerfectPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(Console.ReadLine()));
+        }
+
+        private static string Solve(string input)
+        {
+            int n = Convert.ToInt32(input);
+            if (n % 2 == 1) return "-1";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < n; i += 2)
+            {
+                stringBuilder.Append($"{i + 2} {i + 1} ");
+            }
+            stringBuilder.Remove(stringBuilder.Length - 1, 1);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
