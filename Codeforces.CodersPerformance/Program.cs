f61cb37cfb58368840b3f0b8fb62eb4bb0e30b7b
Harshveer Singh <ksharshveer@gmail.com>
2021-01-03 21:13:38 -0800

    Solved ILove%Username%.

---
+++ Codeforces.CodersPerformance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Buffers;
+using System.Linq;
+
+namespace Codeforces.CodersPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = Console.ReadLine();
+            var input = Console.ReadLine().Split()
+                .Select(num => Convert.ToInt32(num))
+                .ToArray();
+
+            Console.WriteLine(GetBestWorstCount(input));
+        }
+
+        private static int GetBestWorstCount(int[] scores)
+        {
+            int bestWorstCount = 0;
+            if (scores.Length == 1) return bestWorstCount;
+
+            int lowestSoFar;
+            int highestSoFar;
+            lowestSoFar = highestSoFar = scores[0];
+
+            foreach (var score in scores)
+            {
+                if (score < lowestSoFar)
+                {
+                    lowestSoFar = score;
+                    bestWorstCount += 1;
+                }
+                if (score > highestSoFar)
+                {
+                    highestSoFar = score;
+                    bestWorstCount += 1;
+                }
+            }
+
+            return bestWorstCount;
+        }
+    }
+}
