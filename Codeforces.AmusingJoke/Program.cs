2a15de8c0559ed2cfc0263a007d816413e1d973f
Harshveer Singh <ksharshveer@gmail.com>
2021-01-12 17:15:10 -0800

    Solved AmusingJoke.

---
+++ Codeforces.AmusingJoke/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.AmusingJoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guest = Console.ReadLine();
+            var host = Console.ReadLine();
+
+            var pileChars = Console.ReadLine().ToCharArray();
+            var guestPlustHostChars = (guest + host).ToCharArray();
+
+            Array.Sort(pileChars);
+            Array.Sort(guestPlustHostChars);
+
+            if (pileChars.SequenceEqual(guestPlustHostChars)) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
