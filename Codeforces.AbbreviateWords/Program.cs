355d93d5c0bd0d7e6487ad29f8acab2fa041a5eb
Harshveer Singh <ksharshveer@gmail.com>
2021-01-12 16:37:03 -0800

    Solved Way Too Long Words.

---
+++ Codeforces.AbbreviateWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Codeforces.AbbreviateWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            string line;
+            for (int i = 0; i < count; i++)
+            {
+                line = Console.ReadLine();
+                PrintWordAbbreviation(ref line);
+            }
+        }
+
+        static void PrintWordAbbreviation(ref string word)
+        {
+            if (word.Length > 10) 
+                Console.WriteLine(word[0].ToString() + (word.Length - 2) + word[word.Length - 1].ToString());
+            else
+                Console.WriteLine(word);
+        }
+    }
+}
