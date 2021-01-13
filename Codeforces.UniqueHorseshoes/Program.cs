4eb07f20f1e968e0b0b21465631099c12e3623c4
Harshveer Singh <ksharshveer@gmail.com>
2021-01-12 16:00:43 -0800

    Solved Is your horseshoe on the other hoof?.

---
+++ Codeforces.UniqueHorseshoes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.UniqueHorseshoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var colors = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            Array.Sort(colors);
+
+            int previousUniqueColor = colors[0];
+            int duplicatesCount = 0;
+
+            for (int i = 1; i < colors.Length; i++)
+            {
+                if (colors[i] == previousUniqueColor) duplicatesCount += 1;
+                else previousUniqueColor = colors[i];
+            }
+
+            Console.WriteLine(duplicatesCount);
+        }
+    }
+}
