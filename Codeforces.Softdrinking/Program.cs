664512fcadb0069eb93b4b04daf5f790d8a21781
Harshveer Singh <ksharshveer@gmail.com>
2021-01-14 19:38:00 -0800

    Solved Soft Drinking.

---
+++ Codeforces.Softdrinking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.Softdrinking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            int drinksForToasts = (numbers[1] * numbers[2]) / numbers[6];
+            int limesForToasts = numbers[3] * numbers[4];
+            int saltForToasts = numbers[5] / numbers[7];
+
+            int min = new[] { drinksForToasts, limesForToasts, saltForToasts }.Min();
+            Console.WriteLine(min / numbers[0]);
+        }
+    }
+}
