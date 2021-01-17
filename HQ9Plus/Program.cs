fc76408e8e4768c25e0e98788a77dd2aaa1acf86
Harshveer Singh <ksharshveer@gmail.com>
2021-01-16 17:49:15 -0800

    Solved HQ9+.

---
+++ HQ9Plus/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HQ9Plus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WillOutput());
+        }
+
+        private static string WillOutput()
+        {
+            var chars = Console.ReadLine().ToCharArray();
+            foreach (var c in chars)
+            {
+                if (c == 'H' || c == 'Q' || c == '9')
+                {
+                    return "YES";
+                }
+            }
+            return "NO";
+        }
+    }
+}
