15b9bbb17e53b209c37a68222770570928cb2e2a
Harshveer Singh <ksharshveer@gmail.com>
2021-01-17 19:37:44 -0800

    Solved Team.

---
+++ Codeforces.SolveIfConfident/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.SolveIfConfident
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string KnowsSolution = "1";
+
+            int willWriteSolutions = 0;
+            int canSolveCount;
+
+            int problemsCount = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < problemsCount; i++)
+            {
+                canSolveCount = 0;
+                Console.ReadLine()
+                    .Split()
+                    .ToList()
+                    .ForEach(s => canSolveCount = s.Equals(KnowsSolution) ? canSolveCount + 1 : canSolveCount);
+
+                if (canSolveCount >= 2) willWriteSolutions += 1;
+            }
+
+            Console.WriteLine(willWriteSolutions);
+        }
+    }
+}
