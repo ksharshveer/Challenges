12cbac6d3e0bf54cbf8dfb81c0ad7f63b5c94902
Harshveer Singh <ksharshveer@gmail.com>
2021-01-19 23:54:54 -0800

    Solved Bit++.

---
+++ Codeforces.BitPlusPlus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codeforces.BitPlusPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            int statements = Convert.ToInt32(Console.ReadLine());
+            char operatorValue;
+            for (int i = 0; i < statements; i++)
+            {
+                operatorValue = Console.ReadLine()[1];
+                if (operatorValue == '+') x += 1;
+                else if (operatorValue == '-') x -= 1;
+            }
+
+            Console.WriteLine(x);
+        }
+    }
+}
