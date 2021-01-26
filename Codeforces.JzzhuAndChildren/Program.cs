425ef7d82b0d11e364ae7cfe01592dc79d5d773b
Harshveer Singh <ksharshveer@gmail.com>
2021-01-26 15:38:50 -0800

    Simple solution to Jzzhu and Children.

---
+++ Codeforces.JzzhuAndChildren/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codeforces.JzzhuAndChildren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parameters = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            (int childrenCount, int candiesDist) = (parameters[0], parameters[1]);
+
+            var candiesWanted = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s));
+            Queue<int> needMoreCandies = new Queue<int>(candiesWanted);
+
+            var indicesRange = Enumerable.Range(1, childrenCount);
+            Queue<int> childrenIndex = new Queue<int>(indicesRange);
+
+            var lastChild = GetLastChildToGoHome(candiesDist, needMoreCandies, childrenIndex);
+            Console.WriteLine(lastChild);
+        }
+
+        private static int GetLastChildToGoHome(int candiesGiven, Queue<int> needMoreCandies, Queue<int> childrenIndex)
+        {
+            int child = -1;
+            while (needMoreCandies.Count != 0)
+            {
+                child = childrenIndex.Dequeue();
+                int candiesNeeded = needMoreCandies.Dequeue();
+                candiesNeeded -= candiesGiven;
+                if (candiesNeeded > 0)
+                {
+                    childrenIndex.Enqueue(child);
+                    needMoreCandies.Enqueue(candiesNeeded);
+                }
+            }
+            return child;
+        }
+
+    }
+}
