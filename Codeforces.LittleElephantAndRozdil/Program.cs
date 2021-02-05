26b3c14b33fd226c39a329f50ee513e8a53c2f0c
Harshveer Singh <ksharshveer@gmail.com>
2021-02-04 18:07:10 -0800

    Solved Little Elephant and Rozdil.

---
+++ Codeforces.LittleElephantAndRozdil/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.LittleElephantAndRozdil
+{
+    class Program
+    {
+        const string NotTravellingMsg = "Still Rozdil";
+
+        static void Main(string[] args)
+        {
+            int citiesCount = Convert.ToInt32(Console.ReadLine());
+
+            var travelDistances = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            if (ShouldTravel(citiesCount, travelDistances, out int townNumber))
+            {
+                Console.WriteLine(townNumber);
+            }
+            else Console.WriteLine(NotTravellingMsg);
+
+        }
+
+        private static bool ShouldTravel(int citiesCount, int[] travelDistances, out int townNumber)
+        {
+            townNumber = 1;
+            int minTownDistance = travelDistances.Min();
+            int minCount = travelDistances.Count((d) => d == minTownDistance);
+            if (minCount > 1) return false;
+
+            // Find town number (travel distance + 1)
+            for (int i = 0; i < citiesCount; i++)
+            {
+                if (travelDistances[i] != minTownDistance) townNumber += 1;
+                else break;
+            }
+
+            return true;
+        }
+    }
+}
