c9e1cd888f5b6dad91bf18758949132755e93680
Harshveer Singh <ksharshveer@gmail.com>
2021-02-08 18:06:34 -0800

    Solved Magic Numbers using Regex.

---
+++ Codeforces.MagicNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Codeforces.MagicNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (IsMagicNumber(input))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        private static bool IsMagicNumber(string input)
+        {
+            var pattern = new Regex(@"^((1)|(14)|(144))+$", RegexOptions.Compiled);
+            return pattern.IsMatch(input);
+        }
+    }
+}
