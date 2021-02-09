8dc52b6389d38818f84fd6a8a367a6f9475ee126
Harshveer Singh <ksharshveer@gmail.com>
2021-02-09 14:57:59 -0800

    Solved Translation.

---
+++ Codeforces.Translation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.Translation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            var translatedWord = Console.ReadLine();
+
+            if (IsReversed(word, translatedWord))
+            {
+                Console.WriteLine("YES");
+            }
+            else Console.WriteLine("NO");
+        }
+
+        private static bool IsReversed(string word, string translatedWord)
+        {
+            return word.SequenceEqual(translatedWord.Reverse());
+        }
+    }
+}
