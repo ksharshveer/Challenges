83d992a83b849234c416b50c75a3259fb646e21d
Harshveer Singh <ksharshveer@gmail.com>
2021-02-10 14:02:56 -0800

    Solved Football.

---
+++ Codeforces.Football/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Codeforces.Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetWinner(count));
+        }
+
+        private static string GetWinner(int matches)
+        {
+            string teamA = Console.ReadLine();
+            string teamB = null;
+            int teamACounter = 1;
+
+            for (int i = 1; i < matches; i++)
+            {
+                var team = Console.ReadLine();
+                if (team.Equals(teamA)) teamACounter += 1;
+                else teamB ??= team;
+            }
+
+            if (teamACounter > Math.Floor((decimal)matches / 2)) return teamA;
+            return teamB;
+        }
+    }
+}
