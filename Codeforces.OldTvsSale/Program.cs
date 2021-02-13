ff925e991c5ca0c890363ee1d6f6fe5ddef1abf7
Harshveer Singh <ksharshveer@gmail.com>
2021-02-12 20:02:00 -0800

    Solved Sale.

---
+++ Codeforces.OldTvsSale/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.OldTvsSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            var (numOfTvs, tvsCarryLimit) = (input[0], input[1]);
+
+            var tvPrices = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            Console.WriteLine(MaximumProfitFromTakingOldTvs(numOfTvs, tvsCarryLimit, tvPrices));
+
+        }
+
+        private static int MaximumProfitFromTakingOldTvs(int numOfTvs, int tvsCarryLimit, int[] tvPrices)
+        {
+            Array.Sort(tvPrices);
+
+            int tvsTaken = 0;
+            int negativeMoneySum = 0;
+            int index = 0;
+            while (tvsTaken < tvsCarryLimit)
+            {
+                if (tvPrices[index] >= 0) break;
+                negativeMoneySum -= tvPrices[index];
+                index += 1;
+                tvsTaken += 1;
+            }
+
+            return Math.Abs(negativeMoneySum);
+        }
+    }
+}
