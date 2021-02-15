f8544a34ed980854c5a2659b4ce21214357a640a
Harshveer Singh <ksharshveer@gmail.com>
2021-02-14 18:51:57 -0800

    Solved Extra long factorial using BigInteger.

---
+++ HackerRank.LongFactorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace HackerRank.LongFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLongFactorial(25));
+        }
+
+        private static Dictionary<string, BigInteger> FactorialsCache = new Dictionary<string, BigInteger>();
+        private static BigInteger GetLongFactorial(BigInteger number)
+        {
+            if (number == 0 || number == 1) return 1;
+            if (FactorialsCache.ContainsKey(number.ToString()))
+            {
+                Console.Write('.');
+                return FactorialsCache[number.ToString()];
+            }
+            FactorialsCache[number.ToString()] = number * GetLongFactorial(number - 1);
+            return FactorialsCache[number.ToString()];
+        }
+    }
+}
