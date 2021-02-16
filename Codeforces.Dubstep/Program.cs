6a070d7cf3a2c8cdd2be1b54fc7be55e427c5404
Harshveer Singh <ksharshveer@gmail.com>
2021-02-15 20:30:17 -0800

    Solved Dubstep using Regex.

---
+++ Codeforces.Dubstep/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Codeforces.Dubstep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string dubWord = "WUB";
+            var dubSong = Console.ReadLine();
+            var origSong = RestoreDub(dubSong, dubWord);
+            Console.WriteLine(origSong);
+        }
+
+        private static string RestoreDub(string dubSong, string dubWord)
+        {
+            Regex exp = new Regex($@"({dubWord})+", RegexOptions.Compiled);
+            var res = exp.Replace(dubSong, " ");
+            return res.Trim();
+        }
+
+    }
+}
