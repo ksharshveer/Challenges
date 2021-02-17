419b2580bd12c8b8caf90889306f4db059053b1c
Harshveer Singh <ksharshveer@gmail.com>
2021-02-17 14:30:07 -0800

    Solved The number of positions...
    It got me first.

---
+++ Codeforces.NumberOfPositionsInLine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.NumberOfPositionsInLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            var (people, minUpfront, maxBehind) = (inputs[0], inputs[1], inputs[2]);
+            Console.WriteLine(Math.Min(people-minUpfront, maxBehind+1));
+        }
+    }
+}
