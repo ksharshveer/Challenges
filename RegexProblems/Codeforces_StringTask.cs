7d4dd192aca551a9beccbc779eb52407d6447ca1
Harshveer Singh <ksharshveer@gmail.com>
2021-02-20 00:19:57 -0800

    Solved String task using regex.

---
+++ RegexProblems/Codeforces_StringTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class Codeforces_StringTask : IRunnable
+    {
+        public void Run()
+        {
+            var line = Console.ReadLine();
+            Solve(line);
+        }
+
+        private static void Solve(string line)
+        {
+            var vowelsPat = new Regex(@"[aeiouyAEIOUY]", RegexOptions.Compiled);
+            var sb = new StringBuilder();
+            foreach (var c in line)
+            {
+                if (!vowelsPat.IsMatch(c.ToString()))
+                {
+                    sb.Append('.');
+                    sb.Append(char.ToLower(c));
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
