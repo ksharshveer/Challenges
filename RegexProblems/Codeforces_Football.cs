f88ce4dc0d7418f95495305cf2289c7316cab279
Harshveer Singh <ksharshveer@gmail.com>
2021-02-20 00:19:36 -0800

    Solved Football using regex.

---
+++ RegexProblems/Codeforces_Football.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class Codeforces_Football : IRunnable
+    {
+        public void Run()
+        {
+            var line = Console.ReadLine();
+            Solve(line);
+        }
+
+        private static void Solve(string line)
+        {
+            var pattern = new Regex(@"1{7,}|0{7,}", RegexOptions.Compiled);
+            if (pattern.IsMatch(line)) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
