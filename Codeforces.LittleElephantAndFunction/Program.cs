8bd68cd689a85e398641458e8a7ca6911d57bde7
Harshveer Singh <ksharshveer@gmail.com>
2021-02-20 17:00:38 -0800

    Solved Little Elephant and Function.

---
+++ Codeforces.LittleElephantAndFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforces.LittleElephantAndFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num);
+            if (num != 1)
+            {
+                Enumerable.Range(1, num-1)
+                    .ToList()
+                    .ForEach(n => Console.Write($" {n}"));
+            }
+        }
+    }
+}
