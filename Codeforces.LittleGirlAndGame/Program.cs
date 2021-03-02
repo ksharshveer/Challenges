d693fa786be395072e54137db59c3053bf342f58
Harshveer Singh <ksharshveer@gmail.com>
2021-03-01 18:37:48 -0800

    Solved Little Girl and Game.

---
+++ Codeforces.LittleGirlAndGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codeforces.LittleGirlAndGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            Console.WriteLine(FindWinningPlayer(str));
+        }
+
+        private static string FindWinningPlayer(string str)
+        {
+            HashSet<char> chars = new HashSet<char>();
+            foreach (var c in str)
+            {
+                if (chars.Contains(c)) chars.Remove(c);
+                else chars.Add(c);
+            }
+
+            if (chars.Count != 0 && chars.Count % 2 == 0) return "Second";
+            return "First";
+        }
+    }
+}
