f494a814c4e14bdd388f6cc78f0c6df431142c2d
Harshveer Singh <ksharshveer@gmail.com>
2021-03-11 21:51:05 -0800

    Improved Xenia and Divisiors...
    Lowered memory consumption, and also added some comments.

--- Codeforces.XeniaAndDivisors/Program.cs
+++ Codeforces.XeniaAndDivisors/Program.cs
@@ -6,33 +6,42 @@ namespace Codeforces.XeniaAndDivisors
 {
     class Program
     {
+        static readonly List<string> PossibleGroups = new List<string> { "1 2 4", "1 2 6", "1 3 6" };
+
         static void Main(string[] args)
         {
-            ReadInput(out int count, out List<int> numbers);
-            if (GroupsExist(numbers, out List<List<int>> groups))
+            ReadInput(out int count, out IEnumerable<int> numbers);
+
+            if (GroupsExist(numbers, out int[] possibleGroupsCounts))
             {
-                foreach (var g in groups)
+                for (int g = 0; g < possibleGroupsCounts.Length; g++)
                 {
-                    Console.WriteLine(string.Join(" ", g.OrderBy(n => n)));
+                    for (int i = 0; i < possibleGroupsCounts[g]; i++)
+                    {
+                        Console.WriteLine(PossibleGroups[g]);
+                    }
                 }
             }
             else Console.WriteLine("-1");
         }
 
-        private static bool GroupsExist(List<int> numbers, out List<List<int>> groups)
+        private static bool GroupsExist(IEnumerable<int> numbers, out int[] possibleGroupsCounts)
         {
-            groups = new List<List<int>>();
+            possibleGroupsCounts = new int[3];
 
+            // Just counting numbers. 1st array element counts occurence of 1, and so on
             var numOfNums = new int[7];
             foreach (var num in numbers)
             {
                 numOfNums[num-1]++;
             }
 
+            // Infeasible conditions
             var groupsCount = numOfNums.Count(n => n > 0);
             var onesCount = numOfNums[0];
             if (groupsCount <= 2 || onesCount == 0) return false;
 
+            // Possible solutions
             for (int i = 0; i < onesCount; i++)
             {
                 if (numOfNums[0] > 0 && numOfNums[1] > 0)
@@ -42,13 +51,13 @@ namespace Codeforces.XeniaAndDivisors
                     {
                         found = true;
                         numOfNums[3]--;
-                        groups.Add(new List<int> { 1, 2, 4 });
+                        possibleGroupsCounts[0] += 1;
                     }
                     else if (numOfNums[5] > 0)
                     {
                         found = true;
                         numOfNums[5]--;
-                        groups.Add(new List<int> { 1, 2, 6 });
+                        possibleGroupsCounts[1] += 1;
                     }
                     if (found)
                     {
@@ -62,22 +71,22 @@ namespace Codeforces.XeniaAndDivisors
                     numOfNums[0]--;
                     numOfNums[2]--;
                     numOfNums[5]--;
-                    groups.Add(new List<int> { 1, 3, 6 });
+                    possibleGroupsCounts[2] += 1;
                 }
             }
 
+            // Just confirming that if solutions exists, no ungrouped items were left
             if (numOfNums.Count(n => n > 0) > 0) return false;
 
             return true;
         }
 
-        private static void ReadInput(out int count, out List<int> numbers)
+        private static void ReadInput(out int count, out IEnumerable<int> numbers)
         {
             count = Convert.ToInt32(Console.ReadLine());
             numbers = Console.ReadLine()
                     .Split(' ')
-                    .Select(s => Convert.ToInt32(s))
-                    .ToList();
+                    .Select(s => Convert.ToInt32(s));
         }
     }
 }
