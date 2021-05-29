0a98ec2e30ba9afefb70eee6be9d39a183e044d3
Harshveer Singh <ksharshveer@gmail.com>
2021-05-29 09:32:26 -0700

    Set framework to solve Two Bags of Potatoes.

---
+++ Codeforces.TwoBagsOfPotatoes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codeforces.TwoBagsOfPotatoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadInput(out int potatoesIn2ndBag, out int divisorK, out int potatoesLimit);
+            if (PossiblePotatoesIn1stBag(potatoesIn2ndBag, divisorK, potatoesLimit, out List<int> potatoCounts))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var pCount in potatoCounts)
+                {
+                    sb.Append(pCount);
+                    sb.Append(" ");
+                }
+
+                var result = sb.ToString().Trim();
+                Console.WriteLine(result);
+            }
+            else Console.WriteLine("-1");
+        }
+
+        private static bool PossiblePotatoesIn1stBag(int potatoesIn2ndBag, int divisorK, int potatoesLimit, out List<int> potatoCounts)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ReadInput(out int potatoesIn2ndBag, out int divisorK, out int potatoesLimit)
+        {
+            var numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(s => Convert.ToInt32(s))
+            .ToArray();
+
+            potatoesIn2ndBag = numbers[0];
+            divisorK = numbers[1];
+            potatoesLimit = numbers[2];
+        }
+    }
+}
